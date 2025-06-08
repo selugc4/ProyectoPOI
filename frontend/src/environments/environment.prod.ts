e89de800f26b40e2c781fd02556d391549d1cadf
Jose Luis Garrido Castro <jgc450@inlumine.ual.es>
2025-06-08 03:28:16 +0200

    fix frontend environment variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyDdFPSH0ZwJNWuR0W12r5bGOQCF8UKhw5Q",
+  authDomain: "storeapp2-fc3a4.firebaseapp.com",
+  projectId: "storeapp2-fc3a4",
+  storageBucket: "storeapp2-fc3a4.firebasestorage.app",
+  messagingSenderId: "71409018255",
+  appId: "1:71409018255:web:73d46fb5fa4fabe2e097fa"
+  }
 };
